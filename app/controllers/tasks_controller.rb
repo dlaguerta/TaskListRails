ce6ad8e1b9559a4b27f132e432d107350f322583
Dianne Laguerta <diannelaguerta@gmail.com>
2016-09-29 15:29:12 -0700

    changed the show controller action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,32 +1,32 @@
 class TasksController < ApplicationController
   def index
-    # @list_of_tasks = Task.all
-    @list_of_tasks = [
-      { id: 1,
-        name:  "finish homework",
-        description: "do the homework!",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      },
-      { id: 2,
-        name: "drink beer",
-        description: "get a lot of beer and drink it",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      },
-      { id: 3,
-        name: "feed foot foot",
-        description: "give the cat lots of food food",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      },
-      { id: 4,
-        name: "deal with the litter box",
-        description: "get that poop out of there",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      }
-    ]
+     @list_of_tasks = Task.all
+    # @list_of_tasks = [
+    #   { id: 1,
+    #     name:  "finish homework",
+    #     description: "do the homework!",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   },
+    #   { id: 2,
+    #     name: "drink beer",
+    #     description: "get a lot of beer and drink it",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   },
+    #   { id: 3,
+    #     name: "feed foot foot",
+    #     description: "give the cat lots of food food",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   },
+    #   { id: 4,
+    #     name: "deal with the litter box",
+    #     description: "get that poop out of there",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   }
+    # ]
 
     # @list_of_tasks = {
     #   # name, description, completion status, and completion date.
@@ -39,33 +39,33 @@ class TasksController < ApplicationController
   end
 
   def show
-    # @list_of_tasks = Task.find(params[:id])
-    @list_of_tasks = [
-      { id: 1,
-        name:  "finish homework",
-        description: "do the homework!",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      },
-      { id: 2,
-        name: "drink beer",
-        description: "get a lot of beer and drink it",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      },
-      { id: 3,
-        name: "feed foot foot",
-        description: "give the cat lots of food food",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      },
-      { id: 4,
-        name: "deal with the litter box",
-        description: "get that poop out of there",
-        completion_status: "not there yet",
-        completion_date: "not there yet"
-      }
-    ]
+     @single_task = Task.find(params[:id])
+    # @list_of_tasks = [
+    #   { id: 1,
+    #     name:  "finish homework",
+    #     description: "do the homework!",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   },
+    #   { id: 2,
+    #     name: "drink beer",
+    #     description: "get a lot of beer and drink it",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   },
+    #   { id: 3,
+    #     name: "feed foot foot",
+    #     description: "give the cat lots of food food",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   },
+    #   { id: 4,
+    #     name: "deal with the litter box",
+    #     description: "get that poop out of there",
+    #     completion_status: "not there yet",
+    #     completion_date: "not there yet"
+    #   }
+    # ]
 
     puts "This is the id: #{params[:id]}"
         @task_id = params[:id].to_i - 1
