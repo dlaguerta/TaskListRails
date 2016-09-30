6addd0ca6ce67083907e9db84b4f6ae216623719
Dianne Laguerta <diannelaguerta@gmail.com>
2016-09-30 11:51:39 -0700

    added a method in the Task model for displaying the task's completion status in view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ActiveRecord::Base
+
+  def task_status
+    if completion_status == true
+      @valid = "complete"
+      "Current Task Status: #{@valid}"
+    else
+      @valid = "incomplete"
+      "Current Task Status: #{@valid}"
+    end
+  end
 end
