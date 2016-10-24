0947ce3dbfca23f7a9f710ae39264ea1e39ef08e
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-23 19:45:56 -0700

    added minitest reporters to gemfile, wrote and passed two tests for sessions controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,22 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+   Minitest::Reporters.use!
+
+  def setup
+    # Turn on test mode - auth requests will be short-circuited
+    OmniAuth.config.test_mode = true
+    # What data should we get back from auth requests?
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github', uid: '99999', info: { email: "a@b.com", name: "Alyssa-Dianne" }
+      })
+  end
+
 end
